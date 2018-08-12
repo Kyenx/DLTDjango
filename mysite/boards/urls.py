2ec91d67a48ac3be4ab863b4a393c3fa5f6bd6db
jkyenkungu <jovin.kyenx@gmail.com>
2018-08-11 23:06:03 -0400

    board topics view added plus template. Tests added too

--- mysite/boards/urls.py
+++ mysite/boards/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'boards'
 urlpatterns = [
     path('', views.index, name='index'),
+    # boards/1/
+    path('boards/<int:pk>/', views.board_topics, name="board_topics")
 ]
--- mysite/boards/views.py
+++ mysite/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 #from django.http import HttpResponse
 from .models import Board
 
@@ -7,3 +7,7 @@ def index(request):
     
     boards = Board.objects.all()
     return render(request, 'boards/index.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)#Board.objects.get(pk=pk)
+    return render(request, 'boards/topics.html', {'board': board})
